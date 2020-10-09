cb45aa8142060c1495fe936fabf9dd4f985a7875
sarwesh.com <sarwesh.com>
2020-10-09 15:18:01 +0530

    program_04= Display memory size of different datatype

---
+++ 04_memorysizeofdatatype.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+printf("memory size of short datatype is %d byte \n",sizeof(short));
+printf("memory size of int datatype is %d byte \n",sizeof(int));
+printf("memory size of float datatype is %d byte \n",sizeof(float));
+printf("memory size of double datatype is %d byte \n",sizeof(double));
+printf("memory size of char datatype is %d byte \n",sizeof(char));
+printf("memory size of 20 char string datatype is %d byte \n",sizeof(char[20]));
+
+
+}
