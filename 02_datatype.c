971ea5bc1806c265d778d23e04343f79483ec515
sarwesh.com <sarwesh.com>
2020-10-09 13:39:33 +0530

    Prog_02= display inti value of all dayatype

---
+++ 02_datatype.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	
+char ch='A';
+char str[20]="hello world";
+float flt=10.234;
+int no=150;
+double dbl=20.1234;
+//clrscr();
+
+printf("character is %c \n",ch);
+printf("string value  is %s \n",str);
+printf("float value is %f \n",flt);
+printf("int value is %d \n",no);
+printf("double value  is %lf \n",dbl);
+
+
+getch();
+}
