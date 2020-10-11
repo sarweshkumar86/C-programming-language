152702247689f608d3984ab4a89d35950cfb3e68
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-10-11 22:32:42 +0530

    prog_05=Display limits of datatypes

---
+++ 05_limitsofdatatype.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+#include<limits.h>
+void main()
+{
+//clrscr();
+printf("signed short min value :%d \n" ,SHRT_MIN);  
+printf("signed short max value :%d \n" ,SHRT_MAX); 
+printf("UNsigned short max value :%u \n" ,USHRT_MAX); 
+printf("signed INT min value :%d \n" ,INT_MIN);  
+printf("signed INT max value :%d \n" ,INT_MAX); 
+printf("UNsigned INT max value :%u \n" ,UINT_MAX); 
+printf("signed LONG min value :%d \n" ,LONG_MIN);  
+printf("signed LONG max value :%d \n" ,LONG_MAX); 
+printf("UNsigned LONG max value :%u \n" ,ULONG_MAX);
+printf("signed CHAR min value :%d \n" ,CHAR_MIN);  
+printf("signed CHAR max value :%d \n" ,CHAR_MAX); 
+printf("UNsigned CHAR max value :%u \n" ,UCHAR_MAX);
+
+getch();
+}
