4fbbb385f4012f6c69282c4c3d4d4aab4b6205fb
sarwesh.com <sarwesh.com>
2020-10-12 15:28:12 +0530

    prog_07=display area of circle

---
+++ 07_areaofcircle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float radius,area;
+    float pi=3.14;
+    //clrscr();
+    printf("please enter the radius the circle \n");
+    scanf("%f",&radius);
+    area=radius*pi;
+    printf("area of cicle  is %.2f",area);
+return (0);
+getch();
+}
