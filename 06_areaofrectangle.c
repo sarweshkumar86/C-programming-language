a0ec53da3f21e111158df5945486f1bc6d111f87
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2020-10-12 06:45:52 +0530

    prog_06=Display area of rectangle

---
+++ 06_areaofrectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float a,b,area;
+    //clrscr();
+    printf("please enter lenght and breadth of a rectangle \n");
+    scanf("%f%f",&a,&b);
+    area=a*b;
+    printf("area of rectangle is %.2f",area);
+return (0);
+getch();
+}
