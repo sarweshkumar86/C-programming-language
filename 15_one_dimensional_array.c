bc3de1306b0ebee0f02e940e867f8dfad74110e6
sarwesh86 <sarweshkumar86@gmail.com>
2020-10-14 13:08:58 +0530

    prog_15= one Dimensional array

---
+++ 15_one_dimensional_array.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i;
+    //clrscr();
+    int arr[5]={10,20,30,40,50};
+
+    for(i=0;i<5;i++)
+    {
+        printf("vlue of array [%d] is =%d \n",i,arr[i]);
+    }
+    getch();
+}
