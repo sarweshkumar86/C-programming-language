f351294ab60df3fd1b0e7e50a35ecac0aa6b2727
sarwesh86 <sarweshkumar86@gmail.com>
2020-10-15 13:55:27 +0530

    prog_17=gets and puts exammple

---
+++ 17_get_puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{    
+    char name[20];
+    //clrscr();
+    printf("Please Enter your name \n");
+    gets(name);
+    puts("your name is");
+    puts(name);
+    getch();
+}
